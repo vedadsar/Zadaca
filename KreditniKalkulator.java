3f3dc2a53bc6db8d36dee54e063b7d6c736f5d2c
Vedad Zornic <vedadsa@windowslive.com>
2014-11-15 13:36:19 +0100

    Kreditni kalkulator.
    Kreditni kalkulator.

---
+++ KreditniKalkulator.java
@@ -0,0 +1,35 @@
+package com.zvedad.zadaca;
+
+import java.util.Scanner;
+
+/** Kreditni kalkulator u kojem korisnik unosi vrijednost glavnice, 
+ * kamatne stope u procentima i broj mjeseci za otplatu. Program ce izracunati
+ * ukupan dug sa kamatom te mjesecnu ratu.
+ * @author Vedad
+ *
+ */
+
+public class KreditniKalkulator {
+	public static void main(String[] args) {
+		double glavnica;
+		double kamata;
+		int mjesec;
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("Unosite vrijednost glavnice ");
+		glavnica = in.nextDouble();
+
+		Scanner in1 = new Scanner(System.in);
+		System.out.println("Unesite procentualnu vrijednost kamatne stope ");
+		kamata = in1.nextDouble();
+
+		Scanner in2 = new Scanner(System.in);
+		System.out.println("Unesite broj mjeseci za otplatu kredita ");
+		mjesec = in2.nextInt();
+        double procKamata = kamata/100;
+		double ukupanDug = glavnica * procKamata + glavnica;
+		System.out.println("Ukupan dug koji cete vrati je " + ukupanDug +" KM");
+		double mjesecnaRata = ukupanDug / mjesec;
+		System.out.println("Mjesecna rata za vas kredit je " + mjesecnaRata +" KM");
+	}
+}
