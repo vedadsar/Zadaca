37a3c67be1ed9b95df1226646b09b92a0dbf6fb1
Vedad Zornic <vedadsa@windowslive.com>
2014-11-16 15:28:55 +0100

    Ispis bilo kojeg broja

---
+++ IspisBiloKojegBroja.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class IspisBiloKojegBroja {
+public static void main(String[] args) {
+	
+	int broj;
+	int cifre;
+		Scanner in = new Scanner(System.in);
+
+	System.out.println("Unesi jedan cijeli broj ");
+	broj = in.nextInt();
+	
+	while(broj != 0){
+		cifre = broj%10;
+		broj = broj/10;
+							
+		System.out.println(cifre);
+	}	
+}
+}
